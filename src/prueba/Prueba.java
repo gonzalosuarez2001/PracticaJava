abdb2b63a2e0ec48cac9cbf80b492af20e4f0a16
Gonzalo <gonzalosuarez9977@gmail.com>
2023-09-05 18:53:24 -0300

    Primer commit

---
+++ src/prueba/Prueba.java
@@ -0,0 +1,28 @@
+package prueba;
+
+public class Prueba {
+
+	public static void main(String[] args) {
+
+		SalaDeCine cine = new SalaDeCine();
+		System.out.println(cine.ocuparButaca(0, 0));
+		System.out.println(cine.ocuparButaca(0, 1));
+		System.out.println(cine.ocuparButaca(0, 2));
+		System.out.println(cine.ocuparButaca(0, 3));
+		System.out.println(cine.ocuparButaca(1, 0));
+		System.out.println(cine.ocuparButaca(1, 1));
+		System.out.println(cine.ocuparButaca(1, 2));
+		System.out.println(cine.ocuparButaca(1, 3));
+		System.out.println(cine.ocuparButaca(2, 0));
+		System.out.println(cine.ocuparButaca(2, 1));
+		System.out.println(cine.ocuparButaca(2, 2));
+		System.out.println(cine.ocuparButaca(2, 3));
+		//System.out.println(cine.ocuparButaca(3, 0));
+		System.out.println(cine.ocuparButaca(3, 1));
+		//System.out.println(cine.ocuparButaca(3, 2));
+		//System.out.println(cine.ocuparButaca(3, 3));
+		System.out.println(cine.hayEspacioPara(3));
+
+	}
+
+}
---
+++ src/prueba/SalaDeCine.java
@@ -0,0 +1,87 @@
+package prueba;
+
+public class SalaDeCine {
+
+	boolean butacas[][];
+
+	public SalaDeCine() {
+		butacas = new boolean[4][4];
+	}
+
+	public boolean estaSalaVacia() {
+
+		boolean estaVacia = true;
+
+		for (int i = 0; i < butacas.length; i++) {
+			for (int j = 0; j < butacas.length; j++) {
+				if (butacas[i][j] != false) {
+					estaVacia = false;
+					return estaVacia;
+				}
+			}
+		}
+		return estaVacia;
+	}
+
+	public boolean estaButacaVacia(int fila, int columna) {
+
+		boolean estaVacia = true;
+
+		if (butacas[fila][columna] != false) {
+			estaVacia = false;
+			return estaVacia;
+		}
+
+		return estaVacia;
+
+	}
+
+	public boolean ocuparButaca(int fila, int columna) {
+
+		boolean seOcupo = false;
+
+		if (butacas[fila][columna] == false) {
+			butacas[fila][columna] = true;
+			seOcupo = true;
+			return seOcupo;
+		}
+		return seOcupo;
+	}
+
+	public int calcularButacasOcupadas() {
+
+		int contador = 0;
+
+		for (int i = 0; i < butacas.length; i++) {
+			for (int j = 0; j < butacas.length; j++) {
+				if (butacas[i][j] == true) {
+					contador++;
+				}
+			}
+		}
+		return contador;
+	}
+
+	public boolean hayEspacioPara(int cantidadDePersonas) {
+
+		boolean hayEspacio = false;
+		int contadorButacasVacias;
+
+		for (int i = 0; i < butacas.length; i++) {
+			contadorButacasVacias = 0;
+			for (int j = 0; j < butacas.length; j++) {
+				if (butacas[i][j] == false) {
+					contadorButacasVacias++;
+				}
+				if (contadorButacasVacias >= cantidadDePersonas) {
+					hayEspacio = true;
+					return hayEspacio;
+				}
+				if (butacas[i][j] == true) {
+					contadorButacasVacias = 0;
+				}
+			}
+		}
+		return hayEspacio;
+	}
+}
